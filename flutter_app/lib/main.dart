eec02013d0a9b4dd0ca99e5b9345c2b46494fbcd
Jordi <jordi.bonet.valiente@lambdaclass.com>
2023-12-28 17:35:48 +0100

    Fix padding

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -178,9 +178,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             const Text('Your photo:',
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold)),
+                                style: TextStyle(fontSize: 16.0)),
                             const SizedBox(height: 8),
                             Expanded(
                               child: Image.file(_image!),
@@ -203,9 +201,8 @@ class _MyHomePageState extends State<MyHomePage> {
                             children: [
                               const Text(
                                   'You are this employee of LambdaClass:',
-                                  style: TextStyle(
-                                      fontSize: 18.0,
-                                      fontWeight: FontWeight.bold)),
+                                  style: TextStyle(fontSize: 16.0)
+                                ),
                               const SizedBox(height: 8),
                               Expanded(
                                 child: Container(
@@ -223,8 +220,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           FloatingActionButton(
             onPressed: _getImage,
@@ -233,6 +231,7 @@ class _MyHomePageState extends State<MyHomePage> {
             backgroundColor: const Color(0xFFB3D334),
             child: const Icon(Icons.add_a_photo),
           ),
+          const SizedBox(height: 16.0),  // Ajusta el espacio entre los botones
           FloatingActionButton(
             onPressed: () {
               // Add the logic for _trainVector
